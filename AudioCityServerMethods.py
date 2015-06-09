0c74308b44c12aba11c49e6651d83e372c3a6078
Piotr Jankowski <pj92@vp.pl>
2015-06-09 17:28:09 +0200

    Stream and route

--- AudioCityServerMethods.py
+++ AudioCityServerMethods.py
@@ -6,17 +6,22 @@ from tkFileDialog import askopenfilename
 from pydub import AudioSegment
 from pydub import playback
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,redirect, url_for
+from werkzeug import secure_filename
+from werkzeug.wrappers import Request, Response
+from werkzeug.wsgi import ClosingIterator, wrap_file
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'files'
+ALLOWED_EXTENSIONS = set(['wav'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
-@app.route('/')
+@app.route('/hello')
 def hello():
-	print "hello"
+	return 'helloWorld'
 	
-@app.route('/upload/', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])  # path upload http://127.0.0.1:3001/upload
 def upload():
 	if request.method == 'POST':
 		file = request.files['file']
@@ -24,51 +29,68 @@ def upload():
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 			a = 'file uploaded'
-			return render_template('upload.html', data = a)
+			return a
+
+			
+@app.route('/stream/<filename>')	# path stream http://127.0.0.1:3001/stream/NazwaPiosenki
+def stream(filename):
+	song = file(os.path.join(os.path.dirname(__file__), 'files', filename))
+	return Response(song, direct_passthrough=True)
 	
-
-
-# http://stackoverflow.com/questions/13386681/streaming-data-with-python-and-flask
-
-
+@app.route('/Path/<filename>')	
+def getFilePath(filename):
+	return os.path.join(os.path.dirname(__file__), 'files', filename)
 	
-	
-# Pydub test
-song = AudioSegment.from_file(filename, format="wav")
-							# Methods on song
-def reverse(song):						
-	song = AudioSegment.from_file(song, format="wav")
-	song = song.reverse()   
+#@app.route('/Audio/<filename>')
+#def getAudio(filename):
+#	return AudioSegment.from_file(getFilePath(filename, format="wav")
+
+@app.route('/Audio/<filename>')
+def getAudio(filename):
+	return AudioSegment.from_file(getFilePath(filename, format="wav")
+	#song = AudioSegment.from_file(getFilePath(filename, format="wav")
 	#return song
-
-def normalization(song):
-	song = AudioSegment.from_file(filename, format="wav")
+	
+#@app.route('/rev/<filename>')
+#def reverse(filename):
+#	song = getAudio(filename)
+#	song = song.reverse()		
+							
+@app.route('/normalization/<filename>')	
+def normalization(filename):
+	song = getAudio(filename)
 	song = song.apply_gain(-sound.max_dBFS) 
-	#return song
-def toMonoStereo(x, song):  # 1 mino , 2 stereo
-	song = AudioSegment.from_file(filename, format="wav")
+
+@app.route('/toMonoStereo/<filename>')	
+def toMonoStereo(x, filename):  # 1 mino , 2 stereo
+	song = getAudio(filename)
 	song = song.set_channels(x)
-	#return song
-def IncresingBeginning(x, song): # 1-10 lub 15 20 s bym zrobil
+	
+@app.route('/IncresingBeginning/<filename>')	
+def IncresingBeginning(x, filename): # 1-10 lub 15 20 s bym zrobil
 	x = x*1000
-	song = AudioSegment.from_file(filename, format="wav")
+	song = getAudio(filename)
 	song = song.fade(from_gain=-120.0, start=0, duration=x) # 1000= 1s
 	#return song
-def fadeEnd(x, song): # tu tak jak w powyzszym
+@app.route('/fadeEnd/<filename>')	
+def fadeEnd(x,filename): # tu tak jak w powyzszym
 	x = x*1000
-	song = AudioSegment.from_file(filename, format="wav")
+	song = getAudio(filename)
 	song = song.fade(to_gain=-120.0, end=0, duration=x) # Wygaszanei koncowki 10s
 	#return song
-def setFrameRate(x, song):
-	song = AudioSegment.from_file(filename, format="wav")
+@app.route('/Framerate/<filename>')	
+def setFrameRate(x,filename):
+	song = getAudio(filename)
 	song = song.set_frame_rate(x) #  # Czestotliwosc wyswietlania klatek  Common values are 44100 (CD), 48000 (DVD), 22050, 24000, 12000 and 11025.
 	#return song
-def setSampleWidth(x, song):
-	song = AudioSegment.from_file(filename, format="wav")
+@app.route('/SampleWidth/<filename>')	
+def setSampleWidth(x,filename):
+	song = getAudio(filename)
 	song = song.set_sample_width(x) # # Ilosc bajtow na probke 1=8 2=16 4=32/64	
 	#return song	
-def speedup(x, song):
-	song = AudioSegment.from_file(filename, format="wav")
+@app.route('/speedup/<filename>')	
+def speedup(x,filename):
+	song = getAudio(filename)
 	song = song.speedup(playback_speed=x, chunk_size=20, crossfade=25)		# Przyspieszenie piosenki 1-2	
 	#return song
 
@@ -78,16 +100,21 @@ def speedup(x, song):
 #song = song.strip_silence(silence_len=100, silence_thresh=-16, padding=100);
 #song = song.speedup(playback_speed=0.5, chunk_size=150, crossfade=25)	# Is slowing possible?	or only 1-2 
 
-												#Converter
-def toMp3(song):
-	song.export("AudioTownSong.mp3", format="mp3") # zapisanie piosenki w formacie mp3
+@app.route('/tomp3/<filename>')													#Converter
+def toMp3(filename):
+	song = getAudio(filename)
+	song.export("AudioTownSong.mp3", format="mp3") # saving mp3
 	#return song
-def toWave(song):
-	song.export("AudioTownSong.wav", format="wav") # zapisanie piosenki w fromacie wav
+@app.route('/towave/<filename>')
+def toWave(filename):
+	song = getAudio(filename)
+	song.export("AudioTownSong.wav", format="wav") # saving wav
 	#return song
  
 						 # Song Analyze 
-def soundAnalyze():						
+@app.route('/soundanalzye/<filename>')
+def soundAnalyze(filename):		
+	song = getAudio(filename)				
 	loudness = song.dBFS  
 	print "Glosnosc piosenki wynosi (dBFS): ",loudness 
   
@@ -105,15 +132,19 @@ def soundAnalyze():
    
 	song.duration_seconds == (len(song) / 1000.0)
 	print "Dlugosc piosenki w sekundach: ",song.duration_seconds
-def volumeup(x,song):
-	song = song.apply_gain(x)  # podglasnianie 0-40
-#louder_via_operator = song + 3.5
-def volumedown(x,song):
-	song = song.apply_gain(x)  # Przyciszanie -40-0
-#quieter_via_operator = song - 5.7
+@app.route('/volumeup/<filename>')
+def volumeup(x,filename):
+	song = getAudio(filename)
+	song = song.apply_gain(x)  #  0-40
+
+@app.route('/volumedown/<filename>')
+def volumedown(x,filename):
+	song = getAudio(filename)
+	song = song.apply_gain(x)  #  -40-0
 
 
-# Streaming
+
+# test chrome-extension://hgmloofddffdnphfgcellkdfbfbjeloo/RestClient.html
 
 
 if __name__ == '__main__':
@@ -122,3 +153,4 @@ if __name__ == '__main__':
         port=int("3001"),
         debug=True
     )
+
